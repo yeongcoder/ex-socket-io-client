ff6af210becb21b69d6e06c3934d60d5bdc72a2b
seo yeongje <seoyeongje@yussiweeogaebaltim-ui-MacBookPro.local>
2021-06-25 11:15:55 +0900

    code prettierrc

--- index.js
+++ index.js
@@ -1,63 +1,56 @@
-const io = require('socket.io-client')
+const io = require("socket.io-client");
 
-const socket = io('http://localhost:3973');
+const socket = io("http://localhost:3353");
 
 let room = null;
 
 process.stdin.resume();
-process.stdin.setEncoding('utf-8');
-process.stdin.on("data", chunk => {
+process.stdin.setEncoding("utf-8");
+process.stdin.on("data", (chunk) => {
+  if (chunk.startsWith("@join")) {
+    let commands = chunk.split(":");
 
-    if(chunk.startsWith("@join")){
+    room = commands[1];
 
-        let commands = chunk.split(":");
-
-        room = commands[1];
-
-        socket.emit("call", "join", { room: room }, (err, res) => {
-            if(err){
-                console.error(err);
-            } else {    
-                console.log("join success")
-            }
-        });
-
-    } else if(chunk.startsWith("@leave")){
-        
-        socket.emit("call", "leave", { room: room }, (err, res) => {
-            if(err){
-                console.error(err);
-            } else {    
-                console.log("leave success")
-            }
-            room = null;
-        })
-
-    } else {
-
-        socket.emit("call", "chat", { text: chunk, room: room }, (err, res) => {
-            if(err){
-                console.error(err);
-            } else {    
-                //
-            }
-        })
-
-    }
-})
-
-socket.on('connect', () => {
-    console.log('socket connect');
+    socket.emit("call", "join", { room: room }, (err, res) => {
+      if (err) {
+        console.error(err);
+      } else {
+        console.log("join success");
+      }
+    });
+  } else if (chunk.startsWith("@leave")) {
+    socket.emit("call", "leave", { room: room }, (err, res) => {
+      if (err) {
+        console.error(err);
+      } else {
+        console.log("leave success");
+      }
+      room = null;
+    });
+  } else {
+    socket.emit("call", "chat", { text: chunk, room: room }, (err, res) => {
+      if (err) {
+        console.error(err);
+      } else {
+        //
+      }
+    });
+  }
 });
-socket.on('disconnect', () => {
-    console.log('socket disconnect');
+
+socket.on("connect", () => {
+  console.log("socket connect");
 });
-socket.on('error', arg => {
-    console.log('socket error: ', arg);
+socket.on("disconnect", () => {
+  console.log("socket disconnect");
 });
-socket.on('noti', (arg) => {
-    console.log('noti: ', arg);
-})
-socket.on('chat', (arg) => {
-    console.log(arg);
-})
+socket.on("error", (arg) => {
+  console.log("socket error: ", arg);
+});
+socket.on("noti", (arg) => {
+  console.log("noti: ", arg);
+});
+socket.on("chat", (arg) => {
+  console.log(arg);
+});
